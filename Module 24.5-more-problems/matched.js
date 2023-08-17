d08941b93ca551ba447f2b85d130c12d42736d9f
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-08-17 22:08:21 +0600

    code updated

---
+++ Module 24.5-more-problems/matched.js
@@ -0,0 +1,13 @@
+const products = [
+    {id: 1, name: "xiaomi", price: "20000"},
+    {id: 2, name: "iPhone", price: "80000"},
+    {id: 3, name: "nexus payment", price: "12000"},
+    {id: 4, name: "web development course", price: "5500"},
+    {id: 5, name: "macbook air", price: "12000"},
+    {id: 6, name: "vallagena", price: "5000"},
+    {id: 7, name: "uiu", price: "39000"},
+    {id: 8, name: "university tuition fees", price: "200000"},
+];
+for(const product of products){
+    console.log(product);
+}
