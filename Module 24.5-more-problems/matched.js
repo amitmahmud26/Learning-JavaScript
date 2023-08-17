a0df2cb4c9c454132553560d71ed8569eab61e7c
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-08-17 22:32:35 +0600

    updated about

--- Module 24.5-more-problems/matched.js
+++ Module 24.5-more-problems/matched.js
@@ -1,13 +1,21 @@
 const products = [
-    {id: 1, name: "xiaomi", price: "20000"},
+    {id: 1, name: "xiaomi phone", price: "20000"},
     {id: 2, name: "iPhone", price: "80000"},
-    {id: 3, name: "nexus payment", price: "12000"},
-    {id: 4, name: "web development course", price: "5500"},
+    {id: 3, name: "nexus phone", price: "12000"},
+    {id: 4, name: "web phone", price: "5500"},
     {id: 5, name: "macbook air", price: "12000"},
-    {id: 6, name: "vallagena", price: "5000"},
-    {id: 7, name: "uiu", price: "39000"},
+    {id: 6, name: "vallagena phone", price: "5000"},
+    {id: 7, name: "uiu phone", price: "39000"},
     {id: 8, name: "university tuition fees", price: "200000"},
 ];
-for(const product of products){
-    console.log(product);
+function matchedProduct(products, search){
+    let matched = [];
+    for(const product of products){
+        if(product.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(product);
+        }
+    }
+    return matched;
 }
+const result = matchedProduct(products, "phone");
+console.log(result);
